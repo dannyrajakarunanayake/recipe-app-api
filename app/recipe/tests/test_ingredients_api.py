51fe27a91f8f24a967ef83ef22c33e4cacfe18e8
danny ferdi <mesh.ferdi@gmail.com>
2020-02-24 12:27:27 +1100

    commit

--- app/recipe/tests/test_ingredients_api.py
+++ app/recipe/tests/test_ingredients_api.py
@@ -55,7 +55,6 @@ class PrivateIngredientsAPITests(TestCase):
             'testpass'
         )
         Ingredient.objects.create(user=user2, name='Vinegar')
-
         ingredient = Ingredient.objects.create(user=self.user, name='tumeric')
 
         res = self.client.get(INGREDIENTS_URL)
@@ -63,3 +62,21 @@ class PrivateIngredientsAPITests(TestCase):
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(len(res.data), 1)
         self.assertEqual(res.data[0]['name'], ingredient.name)
+
+    def test_create_ingredient_successful(self):
+        """Test creating a new ingredient"""
+        payload = {'name': 'Cabbage'}
+        self.client.post(INGREDIENTS_URL, payload)
+
+        exists = Ingredient.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_ingredient_invalid(self):
+        """Test creating invalid ingredient fails"""
+        payload = {'name': ''}
+        res = self.client.post(INGREDIENTS_URL, payload)
+
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
